aaabf765b70f5dcb8b1b5fd64285303419c71c7d
uriqishtepi <uriqishtepi@gmail.com>
2013-10-09 23:08:38 -0400

    this version gets rid of the lower_bound head directly when reading the knight, it works, but it does not return the correct answer

--- dragon_of_loowater.cpp
+++ dragon_of_loowater.cpp
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<iostream>
 #include<algorithm>
+#include<functional>
 #include<vector>
 #include<set>
 #include<map>
 #define forl(i,init, max) for(int i = init; i < max; i++) 
 #define vi std::vector<int>
-#define msi std::multiset<int>
+#define msi std::multiset<int, std::greater<int> >
 #define si std::set<int>
-#define mi std::map<int, itn>
-//#define DEBUG true
+#define mi std::map<int, int>
+#define DEBUG true
 #ifdef DEBUG
 #define out printf
 #else
@@ -44,44 +45,22 @@ int main(int argc, char **argv)
             heads.insert(d);
         }
 
-        vi knights;
         out("knights\n");
-        forl(i, 0, nknights) {
+        forl(i, 0, nknights) { 
             int d = 0;
             scanf("%d\n",&d);
             out("d=%d\n",d);
-            msi::iterator it = heads.find(d);
-            if(it != heads.end()) { //we can get rid of exact values immediately
-                out("found, erasing from heads d=%d\n",d);
-                heads.erase(it);
-                min += d;
+            if(!heads.empty()) {
+                msi::iterator it = heads.lower_bound(d);
+                if(it != heads.end()) { //we can get rid of exact values immediately
+                    out("found, erasing from heads d=%d *it=%d\n",d, *it);
+                    heads.erase(it);
+                    min += d;
+                }
             }
-            else
-                knights.push_back(d);
         }
 
-        std::sort(knights.begin(), knights.end());
-
-        bool solvable = true;
-        vi::iterator last = knights.begin();
-
-        for(msi::iterator it = heads.begin(); it != heads.end(); ++it) {
-            out("considering %d\n", *it );
-            last = std::lower_bound(last, knights.end(), *it);
-            out("distance h %d\n", distance(it, heads.end()));
-            out("distance k %d\n", distance(last, knights.end()));
-
-            if(last == knights.end())
-            {
-                solvable = false;
-                break;
-            }
-
-            min += *last;
-            ++last;
-        }
-
-        if(solvable)
+        if(heads.empty())
             printf("%d\n", min);
         else
             printf("Loowater is doomed!\n");

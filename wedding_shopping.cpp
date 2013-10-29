8820b3383ebd1d4764543d16b48927836f2d4ad3
uriqishtepi <uriqishtepi@gmail.com>
2013-10-28 22:43:47 -0400

    fix an off by one issue with the one that was the fastest.

--- wedding_shopping.cpp
+++ wedding_shopping.cpp
@@ -29,7 +29,6 @@ int main(int argc, char **argv)
     out("Ntests = %d \n", Ntests);
 
     #define MAXMONEY 201
-    int lastmax = 0;
     vi s1;
     s1.reserve(MAXMONEY);
 
@@ -42,7 +41,7 @@ int main(int argc, char **argv)
 
         int perfcounter = 0;
 
-        int slots[MAXMONEY + 1] = {0};
+        int slots[MAXMONEY] = {0};
         s1.clear();
 
         //first element
@@ -61,18 +60,7 @@ int main(int argc, char **argv)
         }
 
         std::sort(s1.begin(), s1.end());
-
-        /*
-        out("s1: ");
-        for(vi::iterator it = s1.begin(); it != s1.end(); ++it) 
-            out(" %d ", *it);
-        out("\n");
-        */
-
-
-        int count = 1;
-
-        while (C > count++) 
+        while (--C > 0) 
         {
             int K = 0;
             scanf("%d\n",&K);
@@ -97,31 +85,16 @@ int main(int argc, char **argv)
             out("\n");
 
             s1.clear();
-            forl(i,0,M) {
+            forl(i,0,M+1) {
                 if(slots[i])
                     s1.push_back(i);
                 slots[i]=0;
             }
-
-            /*
-            out("s1: ");
-            for(vi::iterator it = s1.begin(); it != s1.end(); ++it) 
-                out(" %d ", *it);
-            out("\n");
-            */
-
         }
 
-        /*
-        out("s1: ");
-        for(vi::iterator it = s1.begin(); it != s1.end(); ++it) 
-            out(" %d ", *it);
-        out("\n");
-        */
-
         out("Perfcounter: %d\n", perfcounter);
-        if(!s1.empty() && *s1.rbegin() <= M)
-            printf("%d\n", *s1.rbegin());
+        if(!s1.empty() && s1.back() <= M)
+            printf("%d\n", s1.back());
         else
             printf("no solution\n");
     }

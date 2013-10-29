b4fe1982cf73df35e7596d4d65f117f46ffef6e4
uriqishtepi <uriqishtepi@gmail.com>
2013-10-28 22:30:20 -0400

    go through the processing while reading the input, using slots. this is the fastest so far.

--- wedding_shopping.cpp
+++ wedding_shopping.cpp
@@ -29,10 +29,9 @@ int main(int argc, char **argv)
     out("Ntests = %d \n", Ntests);
 
     #define MAXMONEY 201
-    vi distinct;
-    distinct.reserve(MAXMONEY);
     int lastmax = 0;
-
+    vi s1;
+    s1.reserve(MAXMONEY);
 
     while(0 < Ntests--) 
     {
@@ -42,7 +41,9 @@ int main(int argc, char **argv)
         out("M = %d C =%d\n", M, C);
 
         int perfcounter = 0;
-        si s1;
+
+        int slots[MAXMONEY + 1] = {0};
+        s1.clear();
 
         //first element
         {
@@ -55,9 +56,19 @@ int main(int argc, char **argv)
                 int d = 0;
                 scanf("%d",&d);
                 out(" g%d = %d ", i, d);
-                s1.insert(d);
+                s1.push_back(d);
             }
         }
+
+        std::sort(s1.begin(), s1.end());
+
+        /*
+        out("s1: ");
+        for(vi::iterator it = s1.begin(); it != s1.end(); ++it) 
+            out(" %d ", *it);
+        out("\n");
+        */
+
 
         int count = 1;
 
@@ -66,7 +77,6 @@ int main(int argc, char **argv)
             int K = 0;
             scanf("%d\n",&K);
             out("K = %d ", K);
-            si s2;
 
             forl(i,0,K) {
                 //push v into vector
@@ -74,19 +84,40 @@ int main(int argc, char **argv)
                 scanf("%d",&d);
                 out(" g%d = %d ", i, d);
 
-                for(si::iterator it = s1.begin(); it != s1.end(); ++it) 
+                for(vi::iterator it = s1.begin(); it != s1.end(); ++it) 
                 {
                     int l = *it + d;
                     if(l <= M) {
-                        s2.insert(l);
+                        slots[l] = 1;
                         perfcounter++;
                     }
                     perfcounter++;
                 }
             }
-            s1 = s2;
             out("\n");
+
+            s1.clear();
+            forl(i,0,M) {
+                if(slots[i])
+                    s1.push_back(i);
+                slots[i]=0;
+            }
+
+            /*
+            out("s1: ");
+            for(vi::iterator it = s1.begin(); it != s1.end(); ++it) 
+                out(" %d ", *it);
+            out("\n");
+            */
+
         }
+
+        /*
+        out("s1: ");
+        for(vi::iterator it = s1.begin(); it != s1.end(); ++it) 
+            out(" %d ", *it);
+        out("\n");
+        */
 
         out("Perfcounter: %d\n", perfcounter);
         if(!s1.empty() && *s1.rbegin() <= M)

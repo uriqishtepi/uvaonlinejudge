09d94fea3cedb9ac5fc2907786d671fee2f3f422
uriqishtepi <uriqishtepi@gmail.com>
2013-11-08 08:00:34 -0500

    using sets, cleaned up, seems to work

--- major_scales_10528.cpp
+++ major_scales_10528.cpp
@@ -21,7 +21,7 @@
 #define msi std::map<std::string, int>
 #define INF 1<<30;
 
-#define DEBUG true
+//#define DEBUG true
 #ifdef DEBUG
 #define out printf
 #else
@@ -53,24 +53,38 @@ int main(int argc, char **argv)
     //with the possible ones from the previous notes
     //whene possible ones are empty break
     
-    std::map<int, si > initial; //note to all notes in that chord
-    std::map<int, si > full; //note to all chords that nodes is part
+    std::map<int, si > notesinachord; //note to all notes in that chord
+    std::map<int, si > chordsmakingnote; //note to all chords that nodes is part
     int blah;
     enum { tone = 2, semi_tone = 1};
-    int chord[] = { 0, tone, tone, semi_tone, tone, tone, tone, semi_tone};
+    int chordseq[] = { 0, tone, tone, semi_tone, tone, tone, tone, semi_tone};
 
     forl(i,0,12) {
         int next = i;
 
-        out("full %s: ",notes[i]);
-        forl(j,0,sizeof(chord)/sizeof(int)) {
-            next += chord[j];
-            full[next%12].insert(i); //chords that i takes part in, to i
-            initial[i].insert(next%12);
+        out("processing %s: ",notes[i]);
+        forl(j,0,sizeof(chordseq)/sizeof(int)) {
+            next += chordseq[j];
+            chordsmakingnote[next%12].insert(i); //chords that note i takes part in, to i
+            notesinachord[i].insert(next%12); //note to all notes in that chord
             out( " %s" , notes[next%12] );
         }
         out("\n");
 
+    }
+
+    for(std::map<int, si >::iterator it = chordsmakingnote.begin(); it!= chordsmakingnote.end(); it++) {
+        out("chordsmakingnote %s: ",notes[it->first]);
+        for(si::iterator jt = it->second.begin(); jt != it->second.end(); jt++)
+            out( " %s" , notes[*jt] );
+        out("\n");
+    }
+    
+    for(std::map<int, si >::iterator it = notesinachord.begin(); it!= notesinachord.end(); it++) {
+        out("notesinachord %s: ",notes[it->first]);
+        for(si::iterator jt = it->second.begin(); jt != it->second.end(); jt++)
+            out( " %s" , notes[*jt] );
+        out("\n");
     }
     
 
@@ -86,7 +100,7 @@ int main(int argc, char **argv)
         int count = 0;
 
         char * cptr = strtok(buff, " ");
-        if(cptr == NULL || strcmp(cptr, "END") == 0)
+        if(cptr == NULL || strncmp(cptr, "END",3) == 0)
             continue;
         out("note %s\n", cptr);
 
@@ -94,7 +108,7 @@ int main(int argc, char **argv)
         out("curr %d\n", curr);
 
         //this is the first note, populate the possible set
-        possible = initial[curr];
+        possible = chordsmakingnote[curr];
 
         out("possible size %d: ", possible.size());
         for(si::iterator it = possible.begin();it!=possible.end();++it) {
@@ -104,7 +118,7 @@ int main(int argc, char **argv)
 
 
 
-        while((cptr = strtok(NULL, " ")) != NULL) 
+        while((cptr = strtok(NULL, " \n")) != NULL) 
         {
             out("note %s\n", cptr);
             int curr = revmap[cptr];
@@ -115,34 +129,35 @@ int main(int argc, char **argv)
             while(jt != possible.end() )
             {
                 bool found = false;
-                mmi::iterator it = full.find(*jt);
-                si chords = it->second;
-                while(it != full.end() && it->first == *jt) {
-                    if(curr == it->second) {
-                        found = true;
-                        break;
-                    }
-                    ++it;
-                }    
+                si jtnotes = notesinachord[*jt]; //notes where chord *jt can come from
+                out("jtnotes of %s: ", notes[*jt]);
+                for(si::iterator it = jtnotes.begin();it!=jtnotes.end();++it) {
+                    out( " %s" , notes[*it] );
+                }
+                out("\n");
+
                 si::iterator tmp = jt;
                 ++jt;
 
-                if(!found) {
+                if(jtnotes.find(curr) == jtnotes.end() ) { 
+                    out("NOT found curr %d in jtnotes erasing %d, chord %s from possible\n",curr, *tmp, notes[*tmp]);
                     possible.erase(tmp);
                 }
 
-        out("possible size %d: ", possible.size());
-        for(si::iterator it = possible.begin();it!=possible.end();++it) {
-            out( " %s" , notes[*it] );
-        }
-        out("\n");
-
+                out("possible size %d: ", possible.size());
+                for(si::iterator it = possible.begin();it!=possible.end();++it){
+                    out( " %s" , notes[*it] );
+                }
+                out("\n");
             }
         }
 
         out("Answer possible size %d: ", possible.size());
         for(si::iterator it = possible.begin();it!=possible.end();++it) {
-            std::cout << " " << notes[*it] << " ";
+            if(it == possible.begin())
+                std::cout << notes[*it];
+            else
+                std::cout << " " << notes[*it];
         }
         std::cout << std::endl;
     }

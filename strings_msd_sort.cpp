ef3cf8d08933c8f61e946382092f1e433d1dc6c1
uriqishtepi <uriqishtepi@gmail.com>
2013-12-27 18:59:12 -0500

    adding msd radix sort

---
+++ strings_msd_sort.cpp
@@ -0,0 +1,109 @@
+/* msd radix sort
+ * utilizes counting sort to sort on the leftmost column first, 
+ * then for each subgroup, calls recursively msdsort 
+ * with next letter position
+ * going rightwards until the first column. 
+ * since it can be expensive to sort a few very long strings,
+ * it is commonly optimized to do insertion sort if less than
+ * a certain number of items in subgroup.
+ */
+
+#include <stack>
+#include <queue>
+#include <assert.h>
+#include <vector>
+#include <map>
+#include <queue>
+#include <set>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DEBUG true
+#ifdef DEBUG
+#define out printf
+#else
+#define out
+#endif
+
+int R = 256; //radix 2^8
+
+
+void msd_radix_sort(const std::vector<std::string> & strings, std::vector<int> &v, int start, int end, int chrindx)
+{
+
+  for(int i = start; i < end; i++) {
+      out("%s\n", strings[v[i]].c_str());
+  }
+
+  
+  //do counting sort on the l-th column
+  int counts[256 + 1] = {0};
+
+  for(int i = start; i < end; i++) {
+      int offset = v[i]; //offset of the ith string
+      const std::string & currstr = strings[offset];
+      counts[(currstr.at(chrindx)) + 1]++;
+      out("%d) strings[%d]=%s  counts[%c + 1]=%d \n",i, offset, strings[offset].c_str(), currstr.at(chrindx),  counts[(currstr.at(chrindx)) + 1]);
+  }
+
+  //accumulate the counts
+  for(int i = 0; i < R; i++) {
+      if(counts[i+1] > 0)
+          out("counts[%c]=%d+%d\n",i, counts[i + 1], counts[i]);
+      counts[i + 1] += counts[i];
+  }
+
+  std::vector<int> aux(end-start+1); //map to sorted strings
+  for(int i = start; i < end; i++) {
+      int offset = v[i]; //offset of the ith string
+      const std::string & currstr = strings[offset];
+      aux[start + ++counts[currstr.at(chrindx)]] = v[i];
+      out("aux[%d]=%d\n",start + counts[currstr.at(chrindx)], v[i]);
+  }
+
+  for(int i = start; i < end; i++) {
+      v[i] = start+aux[i];
+  }
+
+  out("chrindx=%d\n",chrindx);
+  for(int i = start; i < end; i++) {
+      out("%d) strings[%d]=%s\n", i, v[i], strings[v[i]].c_str());
+  }
+  
+
+}
+
+
+int main(void)
+{
+  out("starting ...\n");
+  std::cout << " MSD radix sort " << std::endl;
+
+  std::vector<std::string> strings;
+  
+  int N = 0;
+  scanf("%d",&N);
+
+  //start from 
+  for(int i = 0; i < N; i++) {
+      std::string str;
+      std::cin >> str;
+      strings.push_back(str);
+  }
+
+  std::vector<int> v(N); //map to sorted strings
+  for(int i = 0; i < N; i++)
+      v[i] = i; //all v's entries point to initial respective locations 
+
+  msd_radix_sort(strings, v, 0, N, 0);
+
+  for(int i = 0; i < N; i++) {
+      std::cout << strings[v[i]] << std::endl;
+  }
+
+
+  return 0;
+}

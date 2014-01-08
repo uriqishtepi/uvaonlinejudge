e966eca0a8529731f4088ed769ea21ac68fc4e16
uriqishtepi <uriqishtepi@gmail.com>
2014-01-08 13:06:03 +0000

    trying to come up with a smaller dfa for the substring search

--- substring_search_dfa.cpp
+++ substring_search_dfa.cpp
@@ -23,40 +23,81 @@
 
 
 
-struct matrix {
-    matrix(int rows, int cols) : _cols(cols), _rows(rows) 
-    { _v.reserve(rows * cols); }
-    int & operator()(int row, int col) { return _v[row * _cols + col]; }
+struct mydfa {
+    mydfa(int rows, int cols, char * needle) : _cols(cols), _rows(rows)
+    { 
+        _v.reserve(rows * cols); 
+        memset(_charmap, -1, sizeof(_charmap)/sizeof(int));
+        _finalst = strlen(needle);
+
+        _distinct = fillcharmap(needle);
+        build_dfa(needle);
+    }
+    int & operator()(int row, int col) { return offset(row, col); }
+    int & offset(int row, int col) { return _v[row * _cols + col]; }
+
     std::vector<int> _v;
     int _cols;
     int _rows;
-};
+    int _charmap[256];
+    char _revmap[256];
+    int _finalst;
+    int _distinct;
 
-void build_dfa(matrix & dfa, char * str)
-{
-    int finalst = strlen(str);
+    int finalst() { return _finalst; }
+    void build_dfa(char * str)
+    {
+        int lastst = 0;
+        offset(_charmap[str[0]],0) = 1; //starting char moves to state 1
 
-    int lastst = 0;
-    dfa(str[0],0) = 1; //starting char moves to state 1
+        for(int i = 1; i < _finalst; i++) {
+            for(int j = 0; j < _distinct; j++)
+                offset(j, i) = offset(j, lastst);
 
-    for(int i = 1; i < finalst; i++) {
-        for(int j = 0; j < 256; j++)
-            dfa(j, i) = dfa(j, lastst);
-
-        char c = str[i];
-        dfa(c,i) = i + 1;
-        lastst = dfa(c, lastst);
+            char c = str[i]; //_charmap[c] is the state corresponding to character c
+            offset(_charmap[c],i) = i + 1;
+            lastst = offset(_charmap[c], lastst);
+        }
     }
 
-}
+
+    //fill a map of characters with a distinct id, return number of distinct characters
+    //so aba wil have only 2 distinct character
+    int fillcharmap(char * str)
+    {
+        int counter = 0;
+        for(int i = 0; i < _finalst; i++) {
+            if(_charmap[str[i]] < 0) {
+                _revmap[counter] = str[i];
+                _charmap[str[i]] = counter++;
+            }
+        }
+        return counter;
+    }
+
+
+    void print()
+    {
+        for(int i = 0; i < _finalst; i++)
+        {
+            std::cout << _revmap[i] << " " ;
+            for(int j = 0; j < _distinct; j++)
+                std::cout << " " << offset(j, i);
+            std::cout << std::endl;
+        }
+    }
+
+
+};
 
 
 int string_search(char * haystack, char * needle, int & counter)
 {
     int len = strlen(haystack);
     int finalst = strlen(needle);
-    matrix dfa(256, finalst);
-    build_dfa(dfa, needle);
+
+    mydfa dfa(256, needle);
+    dfa.print();
 
     int state = 0;
     int loc = -1;
@@ -64,7 +105,7 @@ int string_search(char * haystack, char * needle, int & counter)
     {
         counter++;
         char c = haystack[i];
-        if((state = dfa(c, state)) == finalst) {
+        if((state = dfa(c, state)) == dfa.finalst()) {
             loc = i - state + 1;
             break;
         }

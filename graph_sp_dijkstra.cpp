437a5d588fca0455b7bf1e7b72c1b94d2953f4d5
uriqishtepi <uriqishtepi@gmail.com>
2014-03-11 22:42:06 -0400

    fixed issues with shortest paths dijkstras
    multiple issues: need to use multimap and multiset to get the
    same weights of different nodes to go in the structures, also
    display error properly when less nodes are passed in.

--- graph_sp_dijkstra.cpp
+++ graph_sp_dijkstra.cpp
@@ -1,3 +1,7 @@
+/* shortest path from a node n to every other node, known as
+ * Dijkstra's algorithm. This is for graphs that have no negative cycles
+ * start from the origin node, and expand via BFS updating shortest path as you go
+ */ 
 #include <stack>
 #include <queue>
 #include <assert.h>
@@ -40,8 +44,8 @@ struct cmpedge {
 #define vi std::vector<int>
 #define vd std::vector<double>
 #define mwp std::map<double, edge> //map weight and destination point 
-#define mdi std::map<double, int>  //map double int
-#define se std::set<edge, cmpedge>  //set of edges
+#define mdi std::multimap<double, int>  //map double int
+#define se std::multiset<edge, cmpedge>  //set of edges
 #define graphtp std::vector< se >  //graph is a vector of maps -- the edges
 #define INFINITY 200000000
 
@@ -90,10 +94,10 @@ void ShortestPath(const graphtp & g, int n)
     D[n] = 0;
 
     //start with current in queue
-    mdi q;
-    edge e; e.from = n; e.to = n; e.weight = 0.0;
-    q.insert(std::make_pair(0,n));
-    out("initial n %d ", n);
+    mdi q; //poor man's queue: map of weight to from_node
+
+    q.insert(std::make_pair(0.0,n));
+    out("initial n %d\n", n);
 
     int counter = 0;
     while(!q.empty()) 
@@ -109,12 +113,16 @@ void ShortestPath(const graphtp & g, int n)
 
         for(se::const_iterator it = g[node].begin(); it != g[node].end(); ++it)
         {
+            if(it->from != node) 
+                printf("%d != %d\n", it->from, node);
             assert(it->from == node && "encountered an inconsistend element");
             out("considering %d -> %d \n", node, it->to);
-            if(visited[it->to]) {
+
+            if(visited[it->to]) { //need to check because q allows dup nodes
                 out("encountered a prev. visited node %d\n", it->to);
                 continue;
             }
+
             float dist = D[node] + it->weight;
             out("dist from %d to %d; old dist %.2f vs new dist %.2f\n", 
                             node, it->to, D[it->to], dist);
@@ -195,6 +203,7 @@ int main(void)
             e.from = nodefrom;
             e.to = nodeto;
             e.weight = weight;
+            out("inserting e %d %d %f\n",e.from, e.to, e.weight);
             me.insert(e);
 
             tok = strtok(NULL, " \n\t");
@@ -203,8 +212,14 @@ int main(void)
         out("size of g %d\n",g.size());
     }
 
-
     printf("Case %d:\n", ++ord);
+
+    if(g.size() != m) {
+        printf("need to have all the nodes explicitly defined, even if no links\n");
+        continue;
+    }
+
+
     print_graph(g);
 
     ShortestPath(g, 0);

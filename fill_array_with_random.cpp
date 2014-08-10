7bb32c73eb9aa2c622c06f00564b455ad5f1a195
uriqishtepi <uriqishtepi@gmail.com>
2014-08-10 01:06:47 -0400

    put some metrics to measure heapify, use vector
    i want to see the difference between inserting one at a time
    which is O(nlogn) and heapify which is O(n). So far i only
    see that we do less comparisons, but the time it takes is
    not proportionally scaled to the logn factor.

--- fill_array_with_random.cpp
+++ fill_array_with_random.cpp
@@ -1,10 +1,15 @@
+/* This is an example of a two way heap how to insert into it 
+ * and how to turn an array with data into a min heap.
+ */
+#include <vector>
 #include <stdlib.h>
-#include<stdio.h>
-#include<assert.h>
+#include <stdio.h>
+#include <assert.h>
 #include <string.h>
+#include <sys/time.h>
 
 #define forl(i,init, max) for(int i = init; i < max; i++) 
-#define SZ (32-1)
+#define SZ (2122332-1)
 
 int swapcounter = 0;
 
@@ -16,41 +21,56 @@ void swap (int &a, int &b)
     swapcounter++;
 }
 
-inline int left(int a) { return 2*(a+1) - 1; }
-inline int right(int a) { return 2*(a+1); }
+#define parent(a) ((a-1)/2)
+inline int left(int a) { return 2*(a+1) - 1; } //right is left + 1
 
-void heapify(int arr[], int sz, int lastel)
+//percolate from el up to sz to have min heap property
+//this way of building the heap takes O(n) (not nlogn)
+void siftDown(std::vector<int> &arr, int sz, int el)
 {
     int min = -1;
     int l = 0;
     int r = 0;
 
-    if((l=left(lastel)) < sz) 
+    if((l=left(el)) < sz) 
         min = l;
-    if((r=right(lastel)) < sz && arr[r] < arr[l])
+    if((r=l+1) < sz && arr[r] < arr[l])
         min = r;
 
-    assert(min < sz && "not possible to be larger than sz");
-    if(arr[min] < arr[lastel]) {
-        swap(arr[min], arr[lastel]);
-        heapify(arr, sz, min);
-    }
+    while(min > el && arr[min] < arr[el]) 
+    {
+        swap(arr[min], arr[el]);
+        el = min;
+
+        if((l=left(el)) < sz) 
+            min = l;
+        if((r=l+1) < sz && arr[r] < arr[l])
+            min = r;
+        assert(min < sz && "not possible to be larger than sz");
+    } 
 }
+
+void heapify(std::vector<int> &arr, int sz)
+{
+    //start at last parent element down to 0, perform siftDown
+    //last element is (SZ-1), and its parent is
+    for(int i = parent(SZ-1); i >= 0; i--) { siftDown(arr, SZ, i); }
+}
+
 
 //assume we want a MIN heap 
-void heap_insert(int arr[], int sz, int lastel)
+void heap_insert(std::vector<int> &arr, int sz, int el)
 {
-    int i = lastel;
-    while(i > 0) {
-        int p = (i-1) / 2;
-        if(arr[p] <= arr[i]) 
+    while(el > 0) {
+        int p = (el-1) / 2;
+        if(arr[p] <= arr[el]) 
             break; //nothing to do
-        swap(arr[p], arr[i]);
-        i = p;
+        swap(arr[p], arr[el]);
+        el = p;
     }
 }
 
-void heap_verify (int arr[], int sz)
+void heap_verify (std::vector<int> &arr, int sz)
 {
     int i = sz - 1;
     while(i > 0) {
@@ -64,21 +84,40 @@ void heap_verify (int arr[], int sz)
     }
 }
 
+
+void timediff(const char * s) {
+    static timeval tv;
+    timeval tmp;
+
+    gettimeofday(&tmp, NULL);
+    int sec = (tmp.tv_sec - tv.tv_sec)*1000000;
+    int msec = (tmp.tv_usec - tv.tv_usec);
+    printf("%20.20s diff = %12.dusec\n", s, sec + msec);
+    tv = tmp;
+}
+
+
 int main (void)
 {
-    int arr[SZ];
-    int arr2[SZ];
+    //int arr[SZ];
+    //int arr2[SZ];
+
+    std::vector<int> arr(SZ);
+
     forl(i, 0, SZ) {
         arr[i] = rand() % SZ*10;
     }
-    memcpy(arr2,arr,sizeof(arr2));
+    //memcpy(arr2,arr,sizeof(arr2));
+    std::vector<int> arr2(arr);
 
     printf("unsorted:\n");
-    forl(i, 0, SZ) { printf("%d %d\n",i, arr[i]); }
+    //forl(i, 0, SZ) { printf("%d %d\n",i, arr[i]); }
 
     swapcounter = 0;
     printf("doing heap insert\n");
+    timediff("starting timer");
     forl(i, 1, SZ) { heap_insert(arr, SZ, i); }
+    timediff("using heap_insert()");
     printf("swapcounter=%d\n",swapcounter);
     printf("sorted1:\n");
     //forl(i, 0, SZ) { printf("%d %d\n",i, arr[i]); }
@@ -86,8 +125,10 @@ int main (void)
 
     //best to start at the end, and substitute with the parent as you go
     swapcounter = 0;
-    printf("doing heapify\n");
-    for(int i = (SZ/2 - 1); i >= 0; i--) { heapify(arr2, SZ, i); }
+    //for each item in the left half, percolate to right to get heap
+    timediff("starting timer");
+    heapify(arr2, SZ);
+    timediff("using heapify()");
     printf("swapcounter=%d\n",swapcounter);
     printf("sorted2:\n");
     //forl(i, 0, SZ) { printf("%d %d\n",i, arr2[i]); }
@@ -95,4 +136,3 @@ int main (void)
 
     return 0;
 }
-

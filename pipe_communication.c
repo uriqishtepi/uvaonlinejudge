10a003037849d57698891a5ad4219851fadcd75c
uriqishtepi <uriqishtepi@gmail.com>
2014-10-14 08:33:36 -0400

    use pipe to get another progs output into ours
    used pipe to have the output from an execve forked program
    to be available for processing by the parent program

--- pipe_communication.c
+++ pipe_communication.c
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,14 +10,10 @@
 
 #define FORL(ii,s,e) for(int ii = s; ii < e; ii++)
 
-/* Example of two processes communicatting via a pipe
+/* Example of two processes child/parent communicatting via a pipe
  */
-
-
-int main()
+int parent_child()
 {
-    //call 10 threads to increase variable passed in
-
     int pipefd[2]; //one for in the other for out
     int rc = pipe2(pipefd, O_NONBLOCK);
     if(-1 == rc) {
@@ -69,3 +66,60 @@ int main()
     return 0;
 }
 
+
+
+/* example where we exec a different command and we pipe it's output
+ * back to this program */
+int main()
+{
+    int pipefd[2];
+
+    int rc = pipe2(pipefd, O_NONBLOCK);
+    if(-1 == rc) {
+        perror("Opening pipe\n");
+        exit(1);
+    }
+    int pid;
+
+    if((pid=fork()) == -1) {
+        perror("Opening pipe\n");
+        exit(1);
+    }
+    else if(0 == pid) { //child
+        while( dup2(pipefd[1], STDOUT_FILENO) == -1 && (errno == EINTR)) {}
+        const char * cmd = "/bin/ls";
+        const char * arg = "/tmp/";
+        close(pipefd[1]);
+        close(pipefd[0]);
+        execl(cmd, cmd, arg);
+        perror("execl"); //if we get here is error
+        exit(1);
+    }
+    //parent
+
+    close(pipefd[1]); //not going to write
+
+    char buffer[4096];
+    while (1) {
+        ssize_t count = read(pipefd[0], buffer, sizeof(buffer));
+        if (count == -1) {
+            continue;
+/*
+            if (errno == EINTR) {
+                continue;
+            } else {
+                perror("read");
+                exit(1);
+            }
+*/
+        } else if (count == 0) {
+            break;
+        } else {
+            write(STDERR_FILENO, buffer, count);
+        }
+    }
+
+    close(pipefd[0]);
+    wait(0);
+    return 0;
+}

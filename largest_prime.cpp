e934364abe174805fbf743d7dbb5623a76148655
uriqishtepi <uriqishtepi@gmail.com>
2014-11-19 18:53:39 -0500

    turned value to unsigned int
    also skip for M that are mult of 2, but that does not
    introduce a gain because they were being stopped early
    by check for % 2 so no gain there.
    Would win function call expense, but apparently even that
    is inlined by compiler without explicitly stating inline.
    I inlined anyway.

--- largest_prime.cpp
+++ largest_prime.cpp
@@ -8,8 +8,8 @@
 #include <stdint.h>
 
 #include <sys/time.h>
-#define forlp(i, a, b, c) for(int i = a; i < b; i+=c) 
-#define forln(i, a, b, c) for(int i = a; i > b; i-=c) 
+#define forlp(i, a, b, c) for(unsigned int i = a; i < b; i+=c) 
+#define forln(i, a, b, c) for(unsigned int i = a; i > b; i-=c) 
 #define forl(i,init, max) for(int i = init; i < max; i++) 
 #define SZ 1000
 #define vp std::vector<point> 
@@ -22,7 +22,7 @@
 #define out
 #endif
 
-inline bool isprime(int n)
+inline bool isprime(unsigned int n)
 {
     if(n <= 0) return false;
     if(n < 4) return true;
@@ -37,9 +37,10 @@ inline bool isprime(int n)
 
 int main (void)
 {
-    int M;
-    scanf("%d\n", &M);
-    forln(i,M,1,1) {
+    unsigned int M;
+    scanf("%u\n", &M);
+    if( (M%2)==0) M--;
+    forln(i,M,1,2) {
         if(isprime(i)) {
             printf("%d\n",i);
             break;

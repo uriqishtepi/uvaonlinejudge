25724396ed95c371bfa48e78588cccfb0f5cecc7
uriqishtepi <uriqishtepi@gmail.com>
2014-11-20 08:36:01 -0500

    using a cache of primes to iterate over to find
    this is slower because it has to search over all numbers
    but maybe modifications can be made to speed it up.

--- largest_prime.cpp
+++ largest_prime.cpp
@@ -13,6 +13,7 @@
 #define forl(i,init, max) for(int i = init; i < max; i++) 
 #define SZ 1000
 #define vp std::vector<point> 
+#define vui std::vector<unsigned int> 
 
 
 //#define DEBUG true
@@ -22,29 +23,36 @@
 #define out
 #endif
 
-inline bool isprime(unsigned int n)
+inline bool isprime(vui & primes, unsigned int n)
 {
     if(n <= 0) return false;
     if(n < 4) return true;
     if((n % 2) == 0) return false;
 
-    forlp(i, 3, n, 2) {
-        //printf("considering %d\n",i);
-        if((n % i) == 0) return false;
+    for(vui::iterator it = primes.begin(); ((*it) * (*it)) <= n; it++) {
+        out("considering %u\n",*it);
+        if((n % *it) == 0) return false;
     }
     return true;
 }
 
 int main (void)
 {
+    vui primes; //use this as cache to store the prime numbers seen so far
+    primes.push_back(2);
+    primes.push_back(3);
     unsigned int M;
     scanf("%u\n", &M);
     if( (M%2)==0) M--;
-    forln(i,M,1,2) {
-        if(isprime(i)) {
-            printf("%d\n",i);
-            break;
+
+    for(unsigned i = 5; i < M;i+=2) 
+    {
+        out("inspecting %u\n",i);
+        if(isprime(primes, i)) {
+            primes.push_back(i);
+            out("adding %u\n",i);
         }
     }
+    printf("%u\n", primes.back());
     return 0;
 }

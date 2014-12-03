bbdaf563ab804b9607a1318a7deadd6bc694a90e
uriqishtepi <uriqishtepi@gmail.com>
2014-12-03 00:14:16 -0500

    create intermediate string only once
    and then print it as many times as needed,
    so that performance would be better

--- lcd_706.cpp
+++ lcd_706.cpp
@@ -30,47 +30,35 @@
 #define out
 #endif
 
-char M [10][6] = {
-   "hb bh", /* 0 */
-   " r r ", /* 1 */
-   "hrhlh", /* 2 */
-   "hrhrh", /* 3 */
-   " bhr ", /* 4 */
-   "hlhrh", /* 5 */
-   "hlhbh", /* 6 */
-   "hr r ", /* 7 */
-   "hbhbh", /* 8 */
-   "hbhrh", /* 9 */
-};
 
-char N [10][3][6] = {
-    /* L       M         R */
-   {" | | ", "-   -", " | | "}, /* 0 */
-   {"     ", "     ", " | | "}, /* 1 */
-   {"   | ", "- - -", " |   "}, /* 2 */
-   {"     ", "- - -", " | | "}, /* 3 */
-   {" |   ", "  -  ", " | | "}, /* 4 */
-   {" |   ", "- - -", "   | "}, /* 5 */
-   {" | | ", "  - -", "   | "}, /* 6 */
-   {"     ", "-    ", " | | "}, /* 7 */
-   {" | | ", "- - -", " | | "}, /* 8 */
-   {" |   ", "- - -", " | | "}, /* 9 */
+char N [10][5][4] = {
+   {" - ", "| |", "   ", "| |", " - " }, /* 0 */
+   {"   ", "  |", "   ", "  |", "   " }, /* 1 */
+   {" - ", "  |", " - ", "|  ", " - " }, /* 2 */
+   {" - ", "  |", " - ", "  |", " - " }, /* 3 */
+   {"   ", "| |", " - ", "  |", "   " }, /* 4 */
+   {" - ", "|  ", " - ", "  |", " - " }, /* 5 */
+   {" - ", "|  ", " - ", "| |", " - " }, /* 6 */
+   {" - ", "  |", "   ", "  |", "   " }, /* 7 */
+   {" - ", "| |", " - ", "| |", " - " }, /* 8 */
+   {" - ", "| |", " - ", "  |", " - " }, /* 9 */
 };
 
 //print portion of a number specified by level
 //number is n, size of number is s
 //hasMore determines whether a blank should follow
-void printNum(int lvl, int s, int n, bool hasMore)
+inline void printNum(char ** str, int lvl, int s, int n, bool hasMore)
 {
     assert(n < 10 && n >= 0);
     assert(lvl < 5 && lvl >= 0);
-    printf("%c", N[n][0][lvl]); //L 
+    ((*str)++)[0] = N[n][lvl][0]; // L
 
     forl(i, 1, s+1) {
-        printf("%c", N[n][1][lvl]); //M 
+        ((*str)++)[0] = N[n][lvl][1]; // M
     }  
-    printf("%c", N[n][2][lvl]); //R 
-    if(hasMore) printf(" ");
+    ((*str)++)[0] = N[n][lvl][2]; // R
+    if(hasMore) 
+        ((*str)++)[0] = ' ';
 }
 
 int main(int argc, char **argv)
@@ -96,22 +84,25 @@ int main(int argc, char **argv)
             digits.push_back(n%10);
         }
         std::reverse(digits.begin(), digits.end());
-        forl(j, 0, 5) {
+        forl(j, 0, 5) { // 5 sections
+            char buf[1024]  = {0};
+            char * str = buf;
+
             if(1 == j || 3 == j) {
+                for(int d = 0; d < digits.size(); d++){
+                    int val = digits[d];
+                    printNum(&str, j, s, val, d != digits.size() - 1);
+                }
                 for(int k = 0; k < s; k++) {
-                    for(int d = 0; d < digits.size(); d++){
-                        int val = digits[d];
-                        printNum(j, s, val, d != digits.size() - 1);
-                    }
-                    printf("\n");
+                    printf("%s\n", buf);
                 }
             }
             else {
                 for(int d = 0; d < digits.size(); d++){
                     int val = digits[d];
-                    printNum(j, s, val, d != digits.size() - 1);
+                    printNum(&str, j, s, val, d != digits.size() - 1);
                 }
-                printf("\n");
+                printf("%s\n", buf);
             }
         }
         printf("\n");

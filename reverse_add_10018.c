ebe7a02733354ec85f179378b16804125ea7d05e
uriqishtepi <uriqishtepi@gmail.com>
2014-12-24 22:01:08 -0500

    version that seems to work

--- reverse_add_10018.c
+++ reverse_add_10018.c
@@ -1,33 +1,72 @@
 #include<stdio.h>
-void itoarr(unsigned char dest[], char destlen, int n)
+#include <string.h>
+
+#define MAXL 100
+
+int charstrAdd(char a[MAXL], char b[MAXL])
 {
-    char i = destlen;
-    if(0 == n) { dest[0] = '0'; return ;}
-    while(i-- > 0 && n > 0) {
-        dest[i] = (n % 10) + '0';
-        n /= 10;
+    int s = 0;
+    char carry = 0;
+    while(s < MAXL && a[s] > 0) {
+        char sum = a[s] + b[s] + carry - 2 * '0';
+        if(sum >= 10) {
+            sum -= 10;
+            carry = 1;
+        }
+        else carry = 0;
+        b[s] = sum + '0';
+        s++;
+    }
+    if(carry > 0 && s < MAXL) b[s] = '1';
+
+    if(s >= MAXL) 
+        return -1;
+    else
+        return 0;
+}
+
+
+void reverse(char a[MAXL], char b[MAXL])
+{
+    memset(b, 0, MAXL);
+    int len = strnlen(a, MAXL);
+    int i;
+    for(i = 0; i < len; i++) {
+        b[len-i-1] = a[i];
     }
 }
 
-void process(long long int num)
+void process(char * str)
 {
     int count = 0;
-    while(++count < 1000 && num < 4294967295) {
-        //get reverse
-
-        //sum = num + reverse
-        //if palindrome done
-        //else num = sum
+    char str1[MAXL];
+    char str2[MAXL];
+    strncpy(str2, str, MAXL);
+    while(++count < 1000) {
+        reverse(str2, str1); /* result in second parameter */
+        if(strncmp(str1, str2, MAXL) == 0) {
+            printf("%d %s\n", count, str1);
+            return;
+        }
+        int rc = charstrAdd(str1, str2); /* result in second parameter */
+        if(rc) break;
     }
 }
 
 int main()
 {
     unsigned int n;
+    char str1[MAXL] = {0}; /* "2345678"; */
+    /* char str2[MAXL];
+    reverse(str1,str2); //result in str2
+    printf("%s + %s ", str1, str2);
+    charstrAdd(str1, str2); //result in str2
+    printf("= %s \n", str2);
+    return; */
     scanf("%u\n", &n);
     while(n-- > 0) {
-        long long int num;
-        scanf("%lld\n", &num);
-        process(num);
+        scanf("%s\n", str1);
+        /* printf("%d\n", n); */
+        process(str1);
     }
 }

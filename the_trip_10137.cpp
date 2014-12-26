64d8482bfe54bb88fe6c32671e8ecf9bcea5ebaf
uriqishtepi <uriqishtepi@gmail.com>
2014-12-25 22:09:06 -0500

    using doubles, but still does not work

--- the_trip_10137.cpp
+++ the_trip_10137.cpp
@@ -35,8 +35,7 @@ int main(int argc, char **argv)
 {
     out("Starting...\n");
 
-    int  m, n; //m rows, n columns
-    int item = 0;
+    int n; 
     while(1) {
         scanf("%d\n", &n);
         out("%d\n",n);
@@ -44,39 +43,39 @@ int main(int argc, char **argv)
             return 0;
 
         assert(n > 0 && n <= 1000);
-        vi l;
-        size_t sz;
-        int  counter = 0;
-        int d=0;
-        int c=0;
-        int cents = 0;
-        long long int s=0;
-        while(counter < n && scanf("%d.%d\n",&d, &c) != EOF) {
-            cents = 100*d + c;
-            out("%d %d\n",counter, cents);
-            assert(cents >= 0 && cents <=1000000);
-            l.push_back(cents);
-            s += cents;
+        vd l;
+        int counter = 0;
+        float f=0;
+        double s=0;
+        while(counter < n && scanf("%f\n",&f) != EOF) {
+            out("%d %f\n",counter, f);
+            assert(f >= 0 && f <=10000);
+            l.push_back(f);
+            s += f;
             counter++;
         }
         out("counter %d s %lf\n", counter, s);
-        long long int avg = s / counter;
-        out("%d/%d = %d\n",s, counter, avg);
+        double avg = s / counter;
+        /*
+        long long unsigned int u = avg*100;
+        avg = u;
+        avg = avg / 100;
+        */
+        out("%lf/%d = %lf\n",s, counter, avg);
 
-        long long int exch = 0;
+        double overpaid = 0;
+        double underpaid = 0;
         for(counter = 0; counter < n; counter++ ) {
-            out("%d %d\n",counter, l[counter]);
-            if(l[counter] < avg)
-                exch += avg - l[counter];
+            double diff = ((int) ((avg - l[counter])*100) ) / 100.0;
+            //double diff = avg - l[counter];
+            out("%d %f %f\n",counter, l[counter], diff);
+            if(diff < 0) overpaid -= diff;
+            else underpaid += diff;
         }
+
         //if(item++ > 0) printf("\n");
-        c = exch % 100;
-        d = exch / 100;
-        printf("$%d.", d);
-        if(c < 10)
-            printf("0%d\n", c);
-        else
-            printf("%d\n", c);
+        //printf("$%.2lf\n", exch);
+        printf("$%.2lf\n", std::max(underpaid, overpaid));
     }
 }
 

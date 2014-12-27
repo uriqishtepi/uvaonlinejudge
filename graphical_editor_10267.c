dccb9783f87b0e3383d1f06af03a94d967790f36
uriqishtepi <uriqishtepi@gmail.com>
2014-12-27 00:50:21 -0500

    adding all but one of the operators

--- graphical_editor_10267.c
+++ graphical_editor_10267.c
@@ -1,40 +1,102 @@
 #include<stdio.h>
+#include <string.h>
 
 int main()
 {
-    char * buff = NULL;
-    size_t sz;
-    int image [250][250] = {0};
-    while(getline(buff, &sz, stdin) > 0) {
-        char * tok = strtok(buff, " \n");
-        char cmd = tok[0];
-
+    char buff[1024];
+    char image[250][250];
+    char cmd;
+    int m = 0;
+    int n = 0;
+    while(scanf("%c ", &cmd) != EOF && cmd != 'X') {
+        printf("c=%c\n", cmd);
         switch(cmd) {
-          case I: 
-            tok = strtok(NULL, " \n");
-            int m = atoi(tok);
-              
-            tok = strtok(NULL, " \n");
-            int n = atoi(tok);
-            memset(0, );
+          case '\n': continue;
+          case 'I': 
+            {
+            int i;
+            memset(image, 0, sizeof(image)); 
+            scanf("%d %d", &m, &n);
+            printf("%d %d", m, n);
+
+            for(i = 0; i < n; i++) 
+                memset(image[i], 'O', m* sizeof(char)); 
+            }
             break;
-          case C: 
+          case 'C': 
+            {
+            int i;
+            for(i = 0; i < n; i++) 
+                memset(image[i], 'O', m* sizeof(char)); 
+            }
             break;
-          case L: 
+          case 'L': 
+            {
+            int x, y;
+            char c;
+            scanf("%d %d %c", &x, &y, &c);
+            image[y-1][x-1] = c;
+            }
             break;
-          case V: 
+          case 'V': 
+            {
+                int x, y1, y2;
+                char c;
+                scanf("%d %d %d %c", &x, &y1,&y2,&c);
+                printf("%d %d %d %c", x, y1, y2, c);
+                int i;
+                for(i = y1; i <= y2; i++) 
+                    image[i-1][x-1] = c;
+            }
             break;
-          case H: 
+          case 'H': 
+            {
+                int x1, x2, y;
+                char c;
+                scanf("%d %d %d %c", &x1, &x2, &y,&c);
+                printf("%d %d %d %c", x1, x2, y, c);
+                int j;
+                for(j = x1; j <= x2; j++) 
+                    image[y-1][j-1] = c;
+            }break;
+          case 'K': 
+            {
+                int x1,x2,y1,y2;
+                char c;
+                scanf("%d %d %d %d %c", &x1, &x2, &y1,&y2,&c);
+                printf("%d %d %d %d %c", x1, x2, y1,y2, c);
+                int i, j;
+                for(i = y1; i <= y2; i++) {
+                    for(j = x1; j <= x2; j++) {
+                        image[i-1][j-1] = c;
+                    }
+                    printf("\n");
+                }
+            }
             break;
-          case K: 
+          case 'F': 
+            {
+                int x, y;
+                char c;
+                scanf("%d %d %c", &x, &y, &c);
+                image[y-1][x-1] = c;
+            }
             break;
-          case F: 
+          case 'S': 
+            {
+                gets(buff); //chew rest of line
+                int i, j;
+                for(i = 0; i < n; i++) {
+                    for(j = 0; j < m; j++) {
+                        printf("%c", image[i][j]);
+                    }
+                    printf("\n");
+                }
+            }
             break;
-          case S: 
-            break;
-          case X:
-            break;
-          default : continue;
+          default : 
+            gets(buff);
+            continue;
         }
     }
 

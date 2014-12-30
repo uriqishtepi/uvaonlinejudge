b1492762875c18b594690b5b24688a45b4f7fe93
uriqishtepi <uriqishtepi@gmail.com>
2014-12-30 15:13:41 -0500

    try speed uup with finding max with binary search
    find max by doubling, then work backwards with binary
    search for the answer. Not accepted tho.

--- cryptography_power.java
+++ cryptography_power.java
@@ -7,16 +7,16 @@ public static void main(String[] args)
 {
     try {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BigInteger one = new BigInteger("1");
+      BigInteger two = new BigInteger("2");
 
       while(true) {
         String l1 = br.readLine();
         String l2 = br.readLine();
-        //System.out.print(l1 + "\n");
-        //System.out.print(l2 + "\n");
+        //System.out.print("l1 " + l1 + "\n");
+        //System.out.print("l2 " + l2 + "\n");
 
         int n = Integer.parseInt(l1);
-        BigInteger one = new BigInteger("1");
-        BigInteger two = new BigInteger("2");
         //BigInteger p = new BigInteger("16");
         BigInteger p = new BigInteger(l2);
         //k^n = p
@@ -25,9 +25,21 @@ public static void main(String[] args)
             System.out.print(p + "\n");
             continue;
         }
+        //first do binary search from 1 to more than limit:
+        BigInteger max = one;
+        //System.out.print("0 max " + max + " " + max.pow(n) + "\n");
+        while(max.pow(n).compareTo(p) < 0) {
+            max = max.add(max);
+            //System.out.print("1 max " + max + " " + max.pow(n) + "\n");
+        }
+        //System.out.print("2 max " + max + "\n");
+        if(max.pow(n).equals(p)) {
+            System.out.print(max + "\n");
+            continue;
+        }
+
         //binary search from min to max
         BigInteger min = one;
-        BigInteger max = p;
         while(max.compareTo(min) == 1) {
             BigInteger mid = min.add(max).divide(two);
 

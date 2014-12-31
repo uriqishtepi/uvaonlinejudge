d313bc264c245ba5bd02c842c6fce4d34b06f10c
uriqishtepi <uriqishtepi@gmail.com>
2014-12-31 18:58:38 -0500

    version that works but times out

--- maximum_sum_108.c
+++ maximum_sum_108.c
@@ -1,77 +1,68 @@
 #include<stdio.h>
 #include<sys/param.h>
 
+int rectangleSum(int arr[100][100], int n, int x, int y, int w, int z)
+{
+    int sum = 0;
+    int i;
+    int j;
+    for(i = 0; i <= w; i++) {
+        for(j = 0; j <= z; j++) {
+            sum += arr[x+i][y+j];
+        }
+    }
+    /* printf("rs %d %d %d %d  %d\n", x,y,w,z, sum); */
+    return sum;
+}
+
+int getMax(int arr[100][100], int n, int x, int y)
+{
+    int maxEl = -100000000;
+    int i;
+    int j;
+    for(i = 0; i < n-x; i++) {
+        for(j = 0; j < n-y; j++) {
+            int sum = rectangleSum(arr, n, x, y, i, j);
+            maxEl = MAX(maxEl, sum);
+        }
+    }
+    return maxEl;
+}
+
 int main()
 {
     int n;
-    while(scanf("%d\n", &n) != EOF) {
-
+    while(scanf("%d\n", &n) != EOF) 
+    {
         int i;
         int j;
-        int h[100][100] = {0}; /* horiz max up to this el */
-        int v[100][100] = {0}; /* vert max up to this el */
-        int r[100][100] = {0}; /* rect max up to this el */
-        int maxEl = 0;
+        int arr[100][100] = {0}; 
+        int maxEl = -10000000;
         for(i = 0; i < n; i++) {
             for(j = 0; j < n; j++) {
-                int el;
-                scanf("%d", &el);
-                if(i == 0 && j == 0) { 
-                    maxEl = el;
-                }
-
-                if(i > 0) {
-                    int s = v[i-1][j] + el;
-                    v[i][j] = s > 0 ? s : el;
-                    maxEl = MAX(maxEl, v[i][j]);
-                }
-                else
-                    v[i][j] = el;
-
-                if(j > 0) {
-                    int s = h[i][j-1] + el;
-                    h[i][j] = s > 0 ? s : el;
-                    maxEl = MAX(maxEl, h[i][j]);
-                }
-                else
-                    h[i][j] = el;
-
-                if(i > 0 && j > 0) {
-                    int s = r[i-1][j-1] + h[i][j-1] + v[i-1][j] + el;
-                    r[i][j] = s > 0 ? s : el;
-                    maxEl = MAX(maxEl, r[i][j]);
-                }
-                else if(i > 0) 
-                    r[i][j] = v[i][j];
-                else if(j > 0)
-                    r[i][j] = h[i][j];
-                else
-                    r[i][j] = el;
+                scanf("%d", &arr[i][j]);
+                maxEl = MAX(maxEl, arr[i][j]);
             }
         }
 
+        /*
         for(i = 0; i < n; i++) {
             for(j = 0; j < n; j++) {
-                printf("%d ", h[i][j]);
+                printf("%2d ", arr[i][j]);
             }
             printf("\n");
         }
+        */
+
+
 
         for(i = 0; i < n; i++) {
             for(j = 0; j < n; j++) {
-                printf("%d ", v[i][j]);
+                int mx = getMax(arr, n, i, j);
+                maxEl = MAX(maxEl, mx);
             }
-            printf("\n");
-        }
-
-        for(i = 0; i < n; i++) {
-            for(j = 0; j < n; j++) {
-                printf("%d ", r[i][j]);
-            }
-            printf("\n");
         }
         printf("%d\n", maxEl);
-
     }
 
     return 0;

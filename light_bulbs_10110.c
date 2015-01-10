f23b580eb2b5e91ff244f1b6f73d0e134732cd3b
uriqishtepi <uriqishtepi@gmail.com>
2015-01-09 22:53:59 -0500

    clean version with unsigned int fix

--- light_bulbs_10110.c
+++ light_bulbs_10110.c
@@ -2,27 +2,12 @@
 
 int main()
 {
-    int n;
-    while(scanf("%d\n", &n) != EOF && n != 0) {
-    /* for(n = 1; n < 300; n++) {  */
+    unsigned int n;
+    while(scanf("%u\n", &n) != EOF && n != 0) {
         int nstat = 1; /* 1-st walk */
-        /*
-        int i;
-        for(i = 2; i <= n; i++) {
-            if(n%i == 0) nstat = !nstat;   
-        }
-        printf("%d ", n);
-        if(nstat) printf("yes\n");
-        else printf("no\n");
-        */
-
-
-        /* printf("%d ", n); */
         int rt = sqrt(n);
         if(rt*rt == n) printf("yes\n");
         else printf("no\n");
-        /*
-        */
     }
 
 

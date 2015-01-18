aec9d3eafbe40ee5e14288a07ed87ba916443526
uriqishtepi <uriqishtepi@gmail.com>
2015-01-18 10:52:42 -0500

    better implomentation of combinations
    however still does not compute correctly, can miss
    so need yet a more refined method.

--- combinations_369.c
+++ combinations_369.c
@@ -5,40 +5,40 @@
 int main()
 {
     int m=0, n=0;
-    while(scanf("%d %d\n", &n, &m) != EOF && n !=0 && m != 0) {
+    while(scanf("%d %d\n", &n, &m) != EOF && !(n ==0 && m == 0)) {
         unsigned long long int res = 1;
         int ni = n;
         int mi = m;
         int nmi = n - m;
         int maxpi = MAX(mi, nmi);
         int minpi = MIN(mi, nmi);
-        int arr[100] = {0};
-        int minpicount = 0;
-        int loopcount = 0;
+        int siper[200] = {0};
+        int poshte[200] = {0};
 
-        while(ni > maxpi) {
-            loopcount++;
-            while(res < (1ULL<<63) && ni > maxpi){
-                res *= ni;
-                ni--;
-            }
-            minpicount = 1;
-            int i = minpi;
-            while(i > 1) {  
-                if(arr[i] == 0 && (res % i) == 0) {
-                    res /= i;
-                    arr[i] = 1;
-                    printf("working with %llu / %d\n", res, i);
+        int i, j;
+        for(i = ni; i > maxpi; i--) siper[i] = i;
+
+        for(i = ni; i > maxpi; i--) {
+            for(j = minpi; j > 1; j--) {
+                if(poshte[j] == 0 && (siper[i] % j) == 0) {
+                    poshte[j] = 1;
+                    siper[i] /= j;
                 }
-                else if((res % i)) {
-                    printf("problem with %llu / %d\n", res, i);
-                }
-                if(arr[i] == 1) { printf("done with %d \n", i); minpicount++; }
-                i--;
             }
         }
+
+        /* after the run all the down slots should be zero */
+        for(j = minpi; j > 1; j--) {
+            if(poshte[j] != 1) {
+                fprintf(stderr, "(%d,%d) can not have split by %d.\n", n, m, j);
+            }
+            assert(poshte[j] == 1);
+        }
+
+        for(i = ni; i > maxpi; i--) {
+             res *= siper[i];
+        }
         printf("%d things taken %d at a time is %llu exactly.\n", n, m, res);
-        if(minpicount != minpi) printf("problem minpicount %d minpi %d loopcount %d\n", minpicount, minpi, loopcount);
     }
     return 0;
 }

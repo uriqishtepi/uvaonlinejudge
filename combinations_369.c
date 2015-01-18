3b05948262156505f60a2212e95e38dd87a62bc8
uriqishtepi <uriqishtepi@gmail.com>
2015-01-18 02:42:12 -0500

    a more elaborate version of computing combinations
    also does not work

--- combinations_369.c
+++ combinations_369.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<assert.h>
 #include<sys/param.h>
 
 int main()
@@ -11,18 +12,33 @@ int main()
         int nmi = n - m;
         int maxpi = MAX(mi, nmi);
         int minpi = MIN(mi, nmi);
+        int arr[100] = {0};
+        int minpicount = 0;
+        int loopcount = 0;
 
         while(ni > maxpi) {
+            loopcount++;
             while(res < (1ULL<<63) && ni > maxpi){
                 res *= ni;
                 ni--;
             }
-            while(minpi > 1 && (res % minpi) == 0) {
-                res /= minpi;
-                minpi--;
+            minpicount = 1;
+            int i = minpi;
+            while(i > 1) {  
+                if(arr[i] == 0 && (res % i) == 0) {
+                    res /= i;
+                    arr[i] = 1;
+                    printf("working with %llu / %d\n", res, i);
+                }
+                else if((res % i)) {
+                    printf("problem with %llu / %d\n", res, i);
+                }
+                if(arr[i] == 1) { printf("done with %d \n", i); minpicount++; }
+                i--;
             }
         }
         printf("%d things taken %d at a time is %llu exactly.\n", n, m, res);
+        if(minpicount != minpi) printf("problem minpicount %d minpi %d loopcount %d\n", minpicount, minpi, loopcount);
     }
     return 0;
 }

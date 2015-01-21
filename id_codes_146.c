ae0305d4efde7d36ea42334783842331a3d69ac7
uriqishtepi <uriqishtepi@gmail.com>
2015-01-20 19:23:13 -0500

    almost working version of id codes
    need to fix the corner cases when
    the change needs to sort the rest of the string

--- id_codes_146.c
+++ id_codes_146.c
@@ -8,7 +8,14 @@ cmpchars(const void *p1, const void *p2)
     char c1 = *(const char*) p1;
     char c2 = *(const char*) p2;
     /* printf("comparing %c %c\n", c1, c2); */
-    return c1 < c2;
+    return c1 > c2;
+}
+
+void swap (char * str, int i, int j)
+{
+    char tmp = str[i];
+    str[i] = str[j];
+    str[j] = tmp;
 }
 
 int main()
@@ -16,15 +23,37 @@ int main()
     int n;
     char str[256];
     char copy[256];
-    while(scanf("%s%n", str, &n) != EOF && str[0] != '#') {
-        strcpy(copy, str);
-        printf("n %d str %s\n", n, str);
-        qsort(str, n, sizeof(char), cmpchars);
+    while(scanf("%s%n\n", str, &n) != EOF && str[0] != '#') {
         printf("n %d str %s\n", n, str);
         if(strcmp(copy, str) == 0) {
             printf("last in sequence.\n");
             continue;
         }
+        /* needd to produce combinations, sort them and find the next 
+        is there a better way? all i need is the next one.
+        so if we have ab, we need ba. so we do one flip...
+
+        if we have abc we need acb:
+            abc acb bac bca cab cba
+
+        abcd abdc bacd cabd cbad dabc dbac dbca dcab dcba
+        if we have ababb -> abbab -> abbba
+        start from the end, find the first letter pair that needs flipping
+        then sort from that point to the end
+        */
+        int i;
+        for(i = n-1; i >0; i--) {
+            if(str[i-1] < str[i])
+                break;
+                /* sort from i+i to n */
+        }
+        if(i > 0) {
+            /*find actual min */
+            swap(str,i-1,i);
+            if(n - i > 1)
+                qsort(str+i, n-i, sizeof(char), cmpchars);
+        }
+        printf("n %d str %s\n", n, str);
     }
 
     return 0;

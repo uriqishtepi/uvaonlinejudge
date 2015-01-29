c7bae681e2ef08b135502bb43b20da236f57c7e5
uriqishtepi <uriqishtepi@gmail.com>
2015-01-28 19:02:21 -0500

    doing bfs on each marked unvisited node

--- grid_patches_572.c
+++ grid_patches_572.c
@@ -1,23 +1,53 @@
 #include<stdio.h>
+#include<assert.h>
 
-#define out printf
+#define out 
 
-inline int isSet(char grid[100][100], int i, int j, int m, int n) {
+inline int isSet(int grid[100][100], int i, int j, int m, int n) {
     if(i >= 0 && j >=0 && i < m && j < n)
         return grid[i][j] == '@';
 
     return 0;
 }
 
+/* breadth first search on this grid location 
+ * caller sets this one node, then we only search its neigbors
+*/
+void bfs(int grid[100][100], int id[100][100], int m, int n, int i, int j)
+{
+    assert(isSet(grid, i, j, m, n) && id[i][j] > 0);
+    /* printG(id, m, n); */
+
+    int x, y;
+    for(x=-1;x<2;x++){
+        for(y=-1;y<2;y++){
+            if(x == 0 && y == 0) continue;
+            if(isSet(grid, i + x, j + y, m, n) && id[i + x][j + y] == 0) {
+                id[i + x][j + y] = id[i][j];
+                bfs(grid, id, m, n, i + x, j + y);
+            }
+        }
+    }
+}
+
+void printG( int grid[100][100], int m, int n) {
+int i, j;
+        for(i=0;i<m;i++) {
+            for(j=0;j<n;j++)
+                out("%c", grid[i][j]);
+            out("\n");
+        }
+}
+
+
 int main()
 {
     int m, n;
-
     while(scanf("%d %d\n", &m, &n) != EOF && m != 0) {
         out("m %d n %d\n", m, n);
         int idCounter = 0;
-        char grid[100][100];
-        int id[100][100];
+        int grid[100][100] = {{0}};
+        int id[100][100] = {{0}};
         int i, j;
 
         for(i=0;i<m;i++) {
@@ -25,33 +55,17 @@ int main()
                 scanf("%c", &grid[i][j]);
             scanf("\n");
         }
+        printG(grid, m, n);
 
         for(i=0;i<m;i++) {
-            for(j=0;j<n;j++)
-                out("%c", grid[i][j]);
-            out("\n");
-        }
-
-        int d;
-        int x, y;
-        for(d = 0; d < m + n; d++) {
-            for(x = 0; x < d; x++) {
-                y = d - x;
-                i = (x < m) ? x : m;
-                j = (y < n) ? y : n;
-                if(!isSet(grid, i, j, m, n)) continue;
-                else if(isSet(grid, i-1, j-1, m, n)) id[i][j] = id[i-1][j-1];
-                else if(isSet(grid, i-1, j, m, n)) id[i][j] = id[i-1][j];
-                else if(isSet(grid, i-1, j+1, m, n)) id[i][j] = id[i-1][j+1];
-                else if(isSet(grid, i, j-1, m, n)) id[i][j] = id[i][j-1];
-                else if(isSet(grid, i-1, j-1, m, n)) id[i][j] = id[i-1][j-1];
-                else id[i][j] = ++idCounter;
+            for(j=0;j<n;j++) {
+                if(isSet(grid, i, j, m, n) && id[i][j] == 0) {
+                    id[i][j] = ++idCounter;
+                    bfs(grid, id, m, n, i, j);
+                }
             }
         }
-
         printf("%d\n", idCounter);
     }
-
-
     return 0;
 }

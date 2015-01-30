579b364a40a81208245b286c1d9c84bb293fec26
uriqishtepi <uriqishtepi@gmail.com>
2015-01-30 18:18:08 -0500

    dont init to zero everytime
    but just set the next location to 0 so
    that the next iteration will work. This should
    give performance boost.

--- product_10106.c
+++ product_10106.c
@@ -37,18 +37,19 @@ void mulDigit(char * res, char * str1, int l1, char c2, int skip)
     char * p3 = res;
     while(skip-- > 0) {
         *p3 = '0';
-        p3++;
+        *(++p3) = '\0';
     }
 
     char carry = 0;
     while(p1 >= str1) {
         char prod = (*p1 - '0') * (c2 - '0') + carry;
         carry = prod / 10; 
-        *p3 = prod - (carry * 10) + '0';
+        *p3++ = prod - (carry * 10) + '0';
         p1--;
-        p3++;
     }
-    if(carry) *p3 = carry + '0';
+    if(carry) *p3++ = carry + '0';
+
+    *p3 = '\0';
 }
 
 unsigned char addDigit(char c1, char c2, char * c3, unsigned char carry)
@@ -79,7 +80,9 @@ void sumNumbers(char * res, char * num1, char * num2)
         carry = addDigit(*p2++, 0, p3++, carry);
 
     if(carry) 
-        *p3 = '1';
+        *p3++ = '1';
+
+    *p3 = '\0';
 }
 
 int main() 
@@ -89,12 +92,14 @@ int main()
     char str2[512];
 
     int n;
+    char res[1024] = {0};
+    char tmp1[1024] = {0};
+    char tmp2[1024] = {0};
     while(scanf("%s\n%s\n", str1, str2) != EOF) {
         if(str1[0] == '0' || str2[0] == '0') {
             printf("0\n");
             continue;
         }
-        char res[1024] = {0};
 
         int l1 = strlen(str1);
         int l2 = strlen(str2);
@@ -115,8 +120,6 @@ int main()
 
         int i;
         for(i = 0; i < mn; i++) {
-            char tmp1[1024] = {0};
-            char tmp2[1024] = {0};
             mulDigit(tmp1, pmx, mx, pmn[i], mn - i - 1);
             sumNumbers(tmp2, res, tmp1);
             memcpy(res, tmp2, sizeof(tmp2));
@@ -126,7 +129,8 @@ int main()
         char * p = res + mx - 1;
         while(*p) p++;
         p--;
-        while(p >= res) printf("%c", *p--);
+        while(p >= res) { printf("%c", *p); *p-- = '\0'; }
+        *p-- = '\0';
         printf("\n");
     }
 

4ca13394b7896eeeeafe51e57ab448d95ae508f4
uriqishtepi <uriqishtepi@gmail.com>
2015-02-09 09:22:11 -0500

    cache all results for speedier answer

--- count_cantor_264.c
+++ count_cantor_264.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+struct pair {
+    int numerator;
+    int denominator;
+};
+#define MAXS 10000000
 int main()
 {
     int i, j, num;
-    while(scanf("%d\n", &num) != EOF) {
-        int count = 0;
-        int down;
-        int up;
-        int direction = 0;
-        for(i = 1; count < num; i++) {
-            for(j = 0; j < i && count < num; count++, j++) {
-                if(direction == 1) {
-                    down = i - j;
-                    up = j + 1;
-                } else {
-                    up = i - j;
-                    down = j + 1;
-                }
+    struct pair * arr = malloc(MAXS * sizeof(struct pair));
+    int count = 0;
+    int down;
+    int up;
+    int direction = 0;
+    for(i = 1; count < MAXS; i++) {
+        for(j = 0; j < i && count < MAXS; count++, j++) {
+            if(direction == 1) {
+                down = i - j;
+                up = j + 1;
+            } else {
+                up = i - j;
+                down = j + 1;
             }
-            direction = !direction;
+
+            arr[count].numerator = up;
+            arr[count].denominator = down;
+
         }
-        printf("TERM %d IS %d/%d\n", count, up, down);
+        direction = !direction;
+    }
+    while(scanf("%d\n", &num) != EOF) {
+        printf("TERM %d IS %d/%d\n", num, arr[num-1].numerator, 
+                arr[num-1].denominator);
     }
     return 0;
 }

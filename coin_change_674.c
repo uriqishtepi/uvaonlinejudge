4a44c6c667b564dd522b3d61f62acca45f616304
uriqishtepi <uriqishtepi@gmail.com>
2015-02-23 09:10:12 -0500

    use multiple for loops to get unique combinations
    this is a bit expensive though, and it seems that i had
    it correctly with the previous version which produced good
    results and was faster.

--- coin_change_674.c
+++ coin_change_674.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
-#define MAXI 127
+#define MAXI 1127
 
 
 int main()
 {
-    int D[MAXI+1] = {0};
-    int i = 0;
-    int c5 = -1;
-    int c10 = -1;
-    int c25 = -1;
-    int c50 = -1;
-    int count = 1;
+    int i, f, q, d, n, c;
     for (i = 0; i < MAXI+1; i++) {
-        if(++c5 == 5) { c5=0; count++; }
-        if(++c10 == 10) { c10=0; count++; }
-        if(++c25 == 25) { c25=0; count++; }
-        if(++c50 == 50) { c50=0; count++; }
-        printf("%d %d %d %d %d %d\n", i, c5, c10, c25, c50, count);
+        int count = 0;
+        for(f = 0; f*50 <= i; f++) {
+            for(q=0; q*25 <= (i-f*50); q++) {
+                for(d=0;d*10 <= (i-f*50-q*25); d++) {
+                    for(n=0; n*5 <= (i-f*50-q*25-d*10); n++){
+                        c = i - -f*50-q*25 - d*10 - n*5;
+                        count++;
+/* printf("%d: %d %d %d %d %d %d\n", i, c, n, d, q, f, count); */
+                    }
+                }
+            }
+        } 
+        printf("%d\n", count);
     }
 
     return 0;

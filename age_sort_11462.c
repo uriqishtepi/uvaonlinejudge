ec184c2631954acc0524b299385be8ee5458e57b
uriqishtepi <uriqishtepi@gmail.com>
2015-02-24 08:40:38 -0500

    fxed end of line space for this one

--- age_sort_11462.c
+++ age_sort_11462.c
@@ -6,21 +6,21 @@ int main()
     int agecount[101] = {0};
     int t;
     int n;
-    int count = 0;
     while(scanf("%d\n", &t) != EOF && t != 0) {
-        if(count++ > 0)
-            printf("\n");
         while(--t >= 0 && scanf("%d ", &n) != EOF) {
             assert(n > 0 && n < 100);
             agecount[n]++;
         }
 
+        int count = 0;
         for(n=1;n<100;n++) {
             while(agecount[n] > 0) {
-                printf("%d ", n);
+                if(count++ != 0) printf(" ");
+                printf("%d", n);
                 agecount[n]--;
             }
         }
+        printf("\n");
     }
 
     return 0;

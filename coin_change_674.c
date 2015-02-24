16068af515770d22083e4a6b82cb9852887ed17a
uriqishtepi <uriqishtepi@gmail.com>
2015-02-23 22:01:39 -0500

    downsample by 5 times, redundancy in computation

--- coin_change_674.c
+++ coin_change_674.c
@@ -1,24 +1,31 @@
 #include<stdio.h>
-#define MAXI 1127
+#define MAXI 500
 
 
 int main()
 {
+    int D[MAXI + 1];
     int i, f, q, d, n, c;
     for (i = 0; i < MAXI+1; i++) {
         int count = 0;
-        for(f = 0; f*50 <= i; f++) {
-            for(q=0; q*25 <= (i-f*50); q++) {
-                for(d=0;d*10 <= (i-f*50-q*25); d++) {
-                    for(n=0; n*5 <= (i-f*50-q*25-d*10); n++){
-                        c = i - -f*50-q*25 - d*10 - n*5;
+        int N = i * 5;
+        for(f = 0; f*50 <= N; f++) {
+            for(q=0; q*25 <= (N-f*50); q++) {
+                for(d=0;d*10 <= (N-f*50-q*25); d++) {
+                    for(n=0; n*5 <= (N-f*50-q*25-d*10); n++){
+                        c = N - f*50-q*25 - d*10 - n*5;
                         count++;
-/* printf("%d: %d %d %d %d %d %d\n", i, c, n, d, q, f, count); */
+                        D[i] = count;
+ /* printf("%d: %d %d %d %d %d %d\n", i, c, n, d, q, f, count); */
                     }
                 }
             }
         } 
-        printf("%d\n", count);
+    }
+
+    int p;
+    while(scanf("%d\n", &p) != EOF) {
+        printf("%d\n", D[p/5]);
     }
 
     return 0;

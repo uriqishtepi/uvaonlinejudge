8dcc91686928650b9b07074f086c2e4d5e0c6002
uriqishtepi <uriqishtepi@gmail.com>
2015-03-20 17:12:38 -0400

    Adding new problem, compute square numbers
    square numbers would be integers with squares integers

---
+++ square_numbers_11461.c
@@ -0,0 +1,36 @@
+/* find square numbers between a and b */
+
+#include<stdio.h>
+#define MX 100000
+
+int binsrch(int sq[], int low, int hi, int a)
+{
+    if (low >= hi) return low;
+
+    int mid = (low + hi) / 2;
+    if(mid == low || mid == hi) return low;
+    if(sq[mid] == a) return mid;
+    if(sq[mid] < a) return binsrch(sq, mid, hi, a);
+    if(sq[mid] > a) return binsrch(sq, low, mid, a);
+}
+
+int main()
+{
+    int sq[333] = {0};
+    int sc = 0;
+    int i;
+    for(i = 0; i*i <= MX; i++) {
+        sq[sc++] = i*i;
+    }
+
+    int a, b;
+    while( scanf("%d %d", &a, &b) != EOF && !(a == 0 && b == 0)) {
+
+        int l1 = binsrch(sq, 0, sc, a);
+        int l2 = binsrch(sq, 0, sc, b);
+        printf("%d\n", l2-l1 +1);
+
+    }
+
+    return 0;
+}

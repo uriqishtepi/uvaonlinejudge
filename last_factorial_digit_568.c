40d16f1b7a974e836648f7558b6f4cc79b65a9f4
uriqishtepi <uriqishtepi@gmail.com>
2015-04-07 23:40:59 -0400

    Fixed issue with problem
    it is not enough to keep one significant digit, not sure if it is two,
    but the solution is to keep as many as we can and still fit answer
    of mult inside of integer, thus 100000 (which * 10000 < MAXINT).

--- last_factorial_digit_568.c
+++ last_factorial_digit_568.c
@@ -1,21 +1,30 @@
 #include<stdio.h>
 
+unsigned int max = 100000;
 
 int main()
 {
-    int N;
+    int N = 10001;
+    int arr[10010] = {0};
+    arr[0] = 1;
+    arr[1] = 1;
+    int i;
+    long long unsigned int fact = 1;
+    unsigned int part = 1;
+    for(i = 2; i <= N; i++) {
+        fact = fact * i;
+        while(fact != 0 && (fact % 10) == 0) fact /= 10;
+
+        part = part * i;
+        while( (part != 0 && part % 10) == 0) part /= 10;
+        if(part > max) part = part % max;
+
+        arr[i] = (int) (part % 10);
+        /* printf("i %d %d fact %llu %d\n", i, arr[i], fact, part); */
+    }
+
     while(scanf("%d", &N) != EOF) {
-        int i;
-        int d = 1;
-        for(i = 2; i <= N; i++) {
-            d = d * i;
-            int rem;
-            while( 0 == (rem = d % 10) ) {
-                d = d / 10;
-            }
-            d = rem;
-        }
-        printf("%5d -> %d\n", N, d);
+        printf("%5d -> %d\n", N, arr[N]);
     }
     return 0;
 }

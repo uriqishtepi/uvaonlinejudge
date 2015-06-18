3c89cc579cd7c3707452793d09b72b2c9702a843
uriqishtepi <uriqishtepi@gmail.com>
2015-06-18 08:53:46 -0400

    Added better solution to cola problem

--- cola_bottles_11150.c
+++ cola_bottles_11150.c
@@ -1,22 +1,30 @@
 #include<stdio.h>
 
+/* b    e    t  r
+ * 1    0    1  1
+ * 2    1    3  0
+ * 3    1    4  1
+ * 4    1+1  6  0
+ * 5    1+1  7  1
+ * 6    2+1  9  0
+ * 7    3   10  1
+ * 8    3+1 12  0
+ * 9    3+1 13  1
+ *10    4+1 15  0
+ */
+
 int main()
 {
     int a[201] = {0};
     int wb[201] = {0}; /*without borrow */
     int rem[201] = {0}; /* orig remainder */
     int i = 0;
+    a[1] = 1;
     
-    for(i=1;i<200;i++) {
-        int currem = i % 3;
-        int q = i / 3;
-        a[i] = i + wb[q];
-        wb[i] = a[i];
-        rem[i] = currem;
-        if(currem + rem[q] > 1) { /* if 2 borrow one */
-            a[i] += 1;
-        }
-        printf("%d: %d\n", i, a[i]);
+    for(i=2;i<=200;i++) {
+        int rem = i % 2;
+        a[i] = a[i-1] + 1 + (!rem);
+        printf("%d %d\n", i, a[i]);
     }
     /*
     int N;

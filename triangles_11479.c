bc2645c27774802b0d5ea19b6753a7f553bb7c9a
zaimi <zaimi@pjeshka.home>
2015-10-31 22:33:59 -0400

    Needed unsigned int for triangles problem

--- triangles_11479.c
+++ triangles_11479.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 
-void mysort(int a[], int numel)
+void mysort(unsigned int a[], int numel)
 {
     int i, j;
     for(i = 0; i < numel-1; i++)
         for(j = i+1; j < numel; j++)
             if(a[i] > a[j]) {
-                int tmp = a[j];
+                unsigned int tmp = a[j];
                 a[j] = a[i];
                 a[i] = tmp;
             }
@@ -15,18 +15,18 @@ void mysort(int a[], int numel)
 
 int main()
 {
-    int n;
-    int cnt;
+    int n = 0;
+    int cnt = 0;
     scanf("%d", &n);
 
     while(cnt++ < n) {
-        int a[3];
-        scanf("%d %d %d", a, a+1, a+2);
+        unsigned int a[3];
+        scanf("%u %u %u", a, a+1, a+2);
         mysort(a, 3);
 
         /*printf("%d %d %d\n", a[0], a[1], a[2]); */
         printf("Case %d: ", cnt);
-        if(a[0] == 0 || a[0] + a[1] < a[2])
+        if(a[0] == 0 || a[0] + a[1] <= a[2])
             printf("Invalid\n");
         else if(a[0] == a[1] && a[1] == a[2])
             printf("Equilateral\n");

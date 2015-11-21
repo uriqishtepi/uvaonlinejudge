b9f45a5195d221122f49db39cc9ebde300ab8275
zaimi <zaimi@pjeshka.home>
2015-11-21 12:20:24 -0500

    substring permutations added few more test cases
    algorithm seems correct but does not pass yet

--- common_permutation_10252.c
+++ common_permutation_10252.c
@@ -1,3 +1,8 @@
+/* find longest string x whose permutation is a subsequence strings a and 
+ * a permutation is a subseq of b
+ *
+ * this means that we need to find longest common characters in a and b
+ */
 
 #include <stdio.h>
 #include <sys/param.h>
@@ -5,7 +10,8 @@
 #define forl(i, S, E) for(i = S; i < E; i++) 
 
 
-void pop_arr(int * arr, char * str) {
+/* given str populate arr with letter frequency */
+void populate_arr(int * arr, char * str) {
     char * p = str;
     while(*p) {
         arr[ (*p) - 'a' ] ++;
@@ -22,8 +28,8 @@ int main()
     while( scanf("%s %s", a, b) != EOF) {
         int arr1[26] = {0};
         int arr2[26] = {0};
-        pop_arr(arr1, a);
-        pop_arr(arr2, b);
+        populate_arr(arr1, a);
+        populate_arr(arr2, b);
 
         int i;
         forl(i,0,26) {

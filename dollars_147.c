6bea14c0d3421a2b536c1ef1cc3673181489fe15
zaimi <zaimi@pjeshka.home>
2015-11-21 11:13:12 -0500

    dollars has issu with evaluator
    there is some issue with conversion because evaluator
    reports segfault or similar.

--- dollars_147.c
+++ dollars_147.c
@@ -10,6 +10,7 @@ int Y[] = {1,2,4,10,20,40,100,200,400,1000,2000};
 #define LVLS      (sizeof(Y)/sizeof(int))
 #define MAXIT     30000
 #define ITEMS     (1 + (MAXIT/5))
+#define debug 0
 
 
 void findM(unsigned long long int M[][LVLS], int curr, int lvl)
@@ -22,14 +23,13 @@ void findM(unsigned long long int M[][LVLS], int curr, int lvl)
             M[curr][lvl] += M[next][t];
         t--;
     }
-    /* printf("%llu ", M[curr][lvl]); */
+    if(debug) printf("%llu ", M[curr][lvl]);
 }
 
 
 int main()
 {
     float fval;
-    int ival;
     unsigned long long int M[ITEMS][LVLS] = {{0}};
     int i;
     int lvl;
@@ -37,14 +37,18 @@ int main()
     for(lvl = 0; lvl < LVLS; lvl++)
         M[0][lvl] = M[1][lvl] = 1;
     for(i = 2; i <= ITEMS; i++) {
-        /* printf("%d: ", i); */
+        if(debug) printf("%d: ", i);
         for(lvl = 0; lvl < LVLS; lvl++)
             findM(M, i, lvl);
-        /* printf("\n"); */
+        if(debug) printf("\n");
     }
 
     /* printf("300.00%17llu\n", M[30000/5][LVLS-1]); */
-    while(scanf("%f", &fval) != EOF && (ival = (int) (fval * 10000) / 100) != 0) {
+    while(scanf("%f", &fval) != EOF)
+    {
+        int ival = (int) (fval * 10000) / 100;
+        if(ival < 1) break;
+
         int md10 = (ival % 10);
         /* printf("md10 %d ival %d ", md10, ival); */
         assert(md10 == 0 || md10 == 5);

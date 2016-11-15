40d70c406de325ef7b7291f6e43b75f851108084
Adi Zaimi <azaimi@bloomberg.net>
2016-11-14 22:53:35 -0500

    No need for mutex and cond var to stop reading, just status done

--- pipe_threads_com.c
+++ pipe_threads_com.c
@@ -11,8 +11,6 @@
 struct readwritepipe {
     int pipefd_w;
     int done;
-    pthread_cond_t can_read;
-    pthread_mutex_t mutex;
 };
 
 
@@ -21,23 +19,23 @@ void * childFunc(void * arg)
     struct readwritepipe * rwp = (struct readwritepipe*) arg;
     int fd = rwp->pipefd_w;
 
-    printf("Child process writing on %d\n", fd);
-    const char * str = "Child sending this";
-    int rc = write(fd, str, strlen(str));
-    if(-1 == rc) {
-        perror("Write Error");
+    for (int i = 0; i < 10; i++) {
+        const char str[256];
+        snprintf(str, sizeof(str), "Child sending this, i=%d\n", i);
+
+        int rc = write(fd, str, strlen(str));
+        if(-1 == rc) {
+            perror("Write Error");
+        }
     }
     rwp->done = 1;
 }
 
-#define FORL(ii,s,e) for(int ii = s; ii < e; ii++)
-
-
-/* Example of two processes child/parent communicatting via a pipe
+/* Example of two threads child/parent communicatting via a pipe
  */
 int main()
 {
-    int pipefd[2]; //one for in the other for out
+    int pipefd[2]; //[0] for IN (read) [1] for OUT (write)
     int rc = pipe2(pipefd, O_NONBLOCK);
     if(-1 == rc) {
         perror("Opening pipe\n");
@@ -46,30 +44,21 @@ int main()
 
     pthread_t thv;
     struct readwritepipe rwp;
-    //pthread_mutex_init(&rwp.mutex, NULL);
-    //pthread_cond_init(&rwp.can_read, NULL);
     rwp.pipefd_w = pipefd[1];
     rwp.done = 0;
 
-    //writes occure on fd 1, reads on 0
-    printf("Child process thould be writing on %d\n", pipefd[1]);
-    pthread_create(&thv, NULL, childFunc, &rwp); //so child will write on fd 1
-    sleep(1);
+    //writes done on spawned thread on fd 1, reads done on 0
+    pthread_create(&thv, NULL, childFunc, &rwp);
 
-
-    //pthread_mutex_lock(&rwp->mutex);
+    char ch;
     while(!rwp.done) { //parent reads on fd 0
-        //int rc = 0;
-        //while(!rwp->done && rc == 0) 
-        //    rc = pthread_cond_wait(&rwp->can_read, &rwp->mutex);
-
-        usleep(100);
-        char ch;
         while((rc = read(pipefd[0], &ch, 1)) > 0)
             write(STDOUT_FILENO, &ch, 1);
-        
-        //pthread_mutex_unlock(&rwp->mutex);
+        usleep(100);
     }
+
+    while((rc = read(pipefd[0], &ch, 1)) > 0)
+        write(STDOUT_FILENO, &ch, 1);
 
     printf("Parent process done\n");
     pthread_join(thv, NULL);
@@ -79,4 +68,3 @@ int main()
 
     return 0;
 }
-
